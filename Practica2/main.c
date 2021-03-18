9136cb4a7f65f9dea96724b91554ea5b5a8f44e8
AlejandroCordero <80540977+AlejandroCordero@users.noreply.github.com>
2021-03-18 19:32:21 +0100

    Final Code (I have to fix the initialization from incompatible pointer type warning))

--- Practica2/main.c
+++ Practica2/main.c
@@ -8,30 +8,20 @@
 #include <stdint.h>												 // Needed for uint8_t
 #include <util/delay.h>
 
-const uint8_t DisplayCC[10] = {0b0111111, 0b0000110,
+const char DisplayCC[10] = {0b0111111, 0b0000110,
 	0b1011011, 0b1001111,
 	0b1100110, 0b1101101,
 	0b1111101, 0b0000111,
 	0b1111111, 0b1101111};
 const int nDigit = 3;											 // Number of digits shown in Display
-float* pot;
-uint8_t* poti;
+unsigned int adc_value;
 // Global variables
 
-
-unsigned int ADC_read()
-{
-	ADMUX=0x40;													 // Channel A0 selected
-	ADCSRA|=(1<<ADSC);											 // Start conversion
-	while(!(ADCSRA & (1<<ADIF)));								 // Wait for ADIF conversion complete return
-	ADCSRA|=(1<<ADIF);											 // Clear ADIF when conversion complete by writing 1
-	return (ADC);												 // Return calculated ADC value
-}														
-// Source: https://circuitdigest.com/microcontroller-projects/how-to-use-adc-in-avr-microcontroller-atmega16
- 
 void ADC_Init() {
 	ADMUX=(1<<REFS1);											 // Selecting internal reference voltage (1.1V)
 	ADCSRA=(1<<ADIE)|(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); // Enable ADC also set Prescaler as 128 and AD interruptions are enabled
+	ADMUX=0x40;													 // Channel A0 selected
+	ADCSRA|=(1<<ADSC);											 // ADSC is set to 1 to start next conversion
 }
 
 void Init_System() {
@@ -39,50 +29,52 @@ void Init_System() {
 
 	DDRC=0b11111111;											 // 7 segments of display are setup as output pins
 	DDRA=0b00000111;											 // 3 digital GND are setup as outputs to multiplex which digit is shown in display
-	DDRF=0b11111110;											 // Potenciometer analog input	
+	DDRF=0b11111110;											 // Potenciometer analog input
 	
 	ADC_Init();													// ADC pins are initialized
-	pot = malloc(sizeof(*pot)*nDigit); 
-	poti = malloc(sizeof(*poti)*nDigit);					    // Source: @YamikaDesu who gave me the idea
 	sei();														// Enables interruptions
 }
 
 int main(void)
 {
 	int j,k;
-	uint8_t* pDisplayCC=&DisplayCC;								// pDisplayCC is initialized to point to the address of DisplayCC
+	char* pDisplayCC=&DisplayCC;								// pDisplayCC is initialized to point to the address of DisplayCC
 	Init_System();
-	pot[0]=(ADC_read()*5.0)/1024;								// First float value is read from ADC input
-	poti[0]=trunc(pot[0]);									    // First float value is truncated to integer
+	float pot[nDigit];
+	char poti[nDigit];
 	while(1) {
 		
-			for (j=0;j<nDigit;j++)
+		for (j=0;j<nDigit;j++)
+		{
+			pot[0]=(adc_value*5.0)/1024;						// First float value is read from ADC input
+			poti[0]=trunc(pot[0]);								// First float value is truncated to integer
+			for (k=1;k<nDigit;k++)								// The following algorithm initializes all values to be shown on display converting each decimal in integer
 			{
-				for (k=1;k<nDigit;k++)							// The following algorithm initializes all values to be shown on display converting each decimal in integer
-				{
-					pot[k]=10*(pot[k-1]-trunc(pot[k-1]));
-					poti[k]=trunc(pot[k]);
-				}
-				
-				if (j==0)										// First digit
-				{
-					PORTC=*(pDisplayCC+poti[j]) | 0x80;
-					PORTA=0xFB;
-				}
-				else
-				{												// Rest of digits
-					PORTC=*(pDisplayCC+poti[j]) & 0x7F;
-					PORTA=(0xFB >> j) | 0xF8;
-				}
+				pot[k]=10*(pot[k-1]-trunc(pot[k-1]));
+				poti[k]=trunc(pot[k]);
 			}
-		}												
+			_delay_ms(1.5);
+			PORTC=0x00;
+			_delay_ms(1.5);
+			
+			if (j==0)											// First digit
+			{
+				PORTC=*(pDisplayCC+poti[j]) | 0x80;
+				PORTA=0xFB;
+			}
+			else
+			{													// Rest of digits
+				PORTC=*(pDisplayCC+poti[j]) & 0x7F;
+				PORTA=(0xFB >> j) | 0xF8;
+			}
+		}
+	}
 	
 	free(pot);													// If somehow while-loop is forced to break, the program stops memory is cleared
-	free(poti);	
+	free(poti);
 }																// Program being interrupted by ISR
 
 ISR(ADC_vect) {
-	pot[0]=ADC_read()*5.0/1024;									// First float value is read from ADC input
-	poti[0]=trunc(pot[0]);										// First float value is truncated to integer
+	adc_value=ADC;
 	ADCSRA |= (1<<ADSC);										// ADSC is set to 1 to start next conversion
 }
